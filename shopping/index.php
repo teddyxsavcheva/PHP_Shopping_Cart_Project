ce491e2d641441fbbcd40faf2aae413ff4adb6bf
Teodora Savcheva <teddyxsavcheva02@gmail.com>
2024-01-11 23:37:18 +0200

    Added a browser icon and changed style.css

--- shopping/index.php
+++ shopping/index.php
@@ -1,9 +1,9 @@
 <?php
-    require_once('./php/createdb.php');
-    require_once('./php/component.php');
+require_once('./php/createdb.php');
+require_once('./php/component.php');
 
-    // Creating instance of CreateDB class
-    $database = new CreateDB("ShoppingCart_DB", "shoppingcart_table");
+// Creating instance of CreateDB class
+$database = new CreateDB("ShoppingCart_DB", "shoppingcart_table");
 ?>
 
 <!DOCTYPE html>
@@ -21,31 +21,36 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <!-- CSS Link -->
-
-
     <link rel="stylesheet" href="./style.css">
+
+    <!-- SVG code for the shopping cart icon -->
+    <link rel="icon" href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' height='12' width='14' viewBox='0 0 576 512'%3E%3Cpath d='M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z' fill='%23FFFFFF'/%3E%3C/svg%3E" type="image/svg+xml">
 </head>
 
 <body>
 
-    <div class="container">
-        <div class="row text-center py-5">
-            <!-- Perfume 1,2,3,4 -->
-            <?php
+    </head>
+
+    <body>
+
+        <div class="container">
+            <div class="row text-center py-5">
+                <!-- Perfume 1,2,3,4 -->
+                <?php
                 $result = $database->displayTable();
                 while ($row = mysqli_fetch_assoc($result)) {
                     perfume($row['product_name'], $row['product_price'], $row['product_image']);
                 }
-            ?>
+                ?>
 
 
+            </div>
         </div>
-    </div>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
-</body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    </body>
 
 </html>
